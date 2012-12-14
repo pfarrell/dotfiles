329fb217992fe403100452d7cafb5d274130a244
Pat Farrell <pfarrell@realgravity.com>
2012-12-14 10:17:26 -0800

    hadoop stuff

--- aws/env.sh
+++ aws/env.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env sh
 
 export EC2_HOME=$HOME/opt/ec2-api-tools
+export EC2_URL=https://ec2.us-east-1.amazonaws.com
+
 export PATH=$PATH:$EC2_HOME/bin
+
 
---
+++ hadoop/env.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+export HADOOP_HOME=~/opt/hadoop
+
+export PATH=$PATH:$HADOOP_HOME/bin
