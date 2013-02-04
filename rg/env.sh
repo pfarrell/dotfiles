eebf1c8894d5a6198dc3041b8c8277c03f1a96a3
Pat Farrell <pfarrell@realgravity.com>
2013-02-04 14:58:18 -0800

    removed local gems

--- rg/env.sh
+++ rg/env.sh
@@ -3,10 +3,9 @@
 #export DYLD_LIBRARY_PATH=”/usr/local/Cellar/mysql/5.5.14/lib:$DYLD_LIBRARY_PATH”
 
 alias ll="ls -lpahtr"
-alias lp="ls -p"
-alias kp="ps auxwww"
 alias ld="bundle exec thor"
-alias ld="bundle exec cap"
+alias bcap="bundle exec cap"
+alias ber="bundle exec ruby"
 #alias vim="mvim -v"
 alias ports="sudo lsof -i -P | grep -i 'listen'"
 
@@ -19,10 +18,10 @@ alias ports="sudo lsof -i -P | grep -i 'listen'"
 #.realgravity/rgi-keypair-20120125.pem
 
 # Morpheus Related
-export LOCAL_GEMS=/Users/pfarrell/proj/gems
-alias ld='bundle exec thor '
-alias m='bundle exec thor '
-alias bcap='bundle exec cap '
+#export LOCAL_GEMS=/Users/pfarrell/proj/gems
+#alias ld='bundle exec thor '
+#alias m='bundle exec thor '
+#alias bcap='bundle exec cap '
 
 export RAILS_ENV=production
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
