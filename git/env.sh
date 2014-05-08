464284f2d012119247906da0a86308d7eaebc85c
Pat Farrell <pfarrell@ulive.com>
2014-05-08 10:18:42 -0700

    git_completion

--- git/env.sh
+++ git/env.sh
@@ -1,1 +1,6 @@
-alias gg='git grep --color'
+alias gg='grep -R --line-number --exclude-dir .git --color'
+
+
+if [ -f /usr/share/git-core/git-completion.bash ]; then
+  . /usr/share/git-core/git-completion.bash
+fi
