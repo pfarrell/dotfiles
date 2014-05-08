b48b40840ea9fcf1143617661c02c534dbe7942c
Pat Farrell <pfarrell@ulive.com>
2014-05-08 10:19:14 -0700

    hbase and hdfs

--- hadoop/env.sh
+++ hadoop/env.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env sh
 
-export HADOOP_HOME=~/opt/hadoop
+export HD_HOME=~/opt/hd
+export HADOOP_HOME=~/opt/hd/hadoop
+export HBASE_HOME=~/opt/hd/hbase
 
-export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/bin:$HBASE_HOME/bin
