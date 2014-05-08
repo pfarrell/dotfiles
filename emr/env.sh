938a9d52fe36bb83d1f1ca763c4d922461918340
Pat Farrell <pfarrell@ulive.com>
2014-05-08 10:21:06 -0700

    default log location for emr stuff

--- emr/env.sh
+++ emr/env.sh
@@ -1,1 +1,4 @@
 export PATH=${PATH}:${HOME}/proj/elastic-mapreduce-ruby
+export HIVE_SCRIPT_DIR=s3://hadoop.testing/scripts
+export HADOOP_OUTPUT_DIR=s3://hadoop.testing/data
+export HADOOP_LOG_DIR=s3://hadoop.testing/logs
