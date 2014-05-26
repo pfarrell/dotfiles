9574eba0077ed8330fc271e42bf68bfeb64abbdd
Pat Farrell <pfarrell@ulive.com>
2014-05-25 23:14:57 -0700

    git-completion for mavericks

--- git/env.sh
+++ git/env.sh
@@ -4,3 +4,7 @@ alias gg='grep -R --line-number --exclude-dir .git --color'
 if [ -f /usr/share/git-core/git-completion.bash ]; then
   . /usr/share/git-core/git-completion.bash
 fi
+
+if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
+  . /usr/local/git/contrib/completion/git-completion.bash
+fi
