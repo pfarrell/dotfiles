81b6bd3775892d83f2522afe3cfb45df62f06f53
Patrick Farrell <pfarrell@prosper.com>
2015-01-15 12:08:16 -0800

    upgrade to java 8

--- java/env.sh
+++ java/env.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home/
 export PATH=$JAVA_HOME/bin:$PATH
