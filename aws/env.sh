43d5953c04baec1d268dd66d9becd8ac022bf4c7
Patrick Farrell <mr.patfarrell@gmail.com>
2015-01-31 20:29:31 -0800

    left coast baby

--- aws/env.sh
+++ aws/env.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 export EC2_HOME=$HOME/opt/ec2-api-tools
-export EC2_URL=https://ec2.us-east-1.amazonaws.com
+export EC2_URL=https://ec2.us-west-2.amazonaws.com
 
 export PATH=$PATH:$EC2_HOME/bin
 
