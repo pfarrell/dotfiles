fcd1c4ad2e7c23127e2383cb83a491f92fa43b24
Patrick Farrell <pfarrell@prosper.com>
2015-02-12 17:33:31 -0800

    added homebrewed git-completion.bas

--- git/env.sh
+++ git/env.sh
@@ -19,6 +19,14 @@ if [ -f /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh ]; 
   . /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
 fi
 
+if [ -f /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-prompt.sh ]; then
+  . /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-prompt.sh
+fi
+
 if [ -f /usr/local/git/contrib/completion/git-prompt.sh ]; then
   . /usr/local/git/contrib/completion/git-prompt.sh
+fi
+
+if [ -f /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-completion.bash ]; then
+  . /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-completion.bash
 fi
