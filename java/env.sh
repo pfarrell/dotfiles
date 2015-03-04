7ac97bb501ee5cefed7dd084e8377828f7005de7
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-04 14:11:08 -0800

    added java configs for prosper development

--- java/env.sh
+++ java/env.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env sh
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home/
-export PATH=$JAVA_HOME/bin:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home
+export ANT_HOME=~/opt/ant
+export GRADLE_HOME=/usr/local/Cellar/gradle/2.2.1
+export MAVEN_HOME=/usr/local/Cellar/maven/3.2.5
+export M2_HOME=/usr/local/Cellar/maven/3.2.5
+export JBOSS_HOME=~/opt/jboss
+export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$MAVEN_HOME/bin:$GRADLE_HOME/bin:$JBOSS_HOME/bin:$PATH
