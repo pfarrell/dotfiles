9b3d869adfd0f7c1f117494d1fd384b0569b6302
Patrick Farrell <mr.patfarrell@gmail.com>
2016-02-16 11:09:31 -0800

    rm old checks

--- git/env.sh
+++ git/env.sh
@@ -3,30 +3,30 @@ alias gpr='git pull --rebase'
 alias gnb='git checkout -b'
 
 
-if [ -f /usr/share/git-core/git-completion.bash ]; then
-  . /usr/share/git-core/git-completion.bash
-fi
+#if [ -f /usr/share/git-core/git-completion.bash ]; then
+#  . /usr/share/git-core/git-completion.bash
+#fi
+#
+#if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
+#  . /usr/local/git/contrib/completion/git-completion.bash
+#fi
+#
+#if [ -f /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash ]; then
+#  . /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
+#fi
+#
+#if [ -f /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh ]; then
+#  . /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
+#fi
+#
+#if [ -f /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-prompt.sh ]; then
+#  . /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-prompt.sh
+#fi
+#
+#if [ -f /usr/local/git/contrib/completion/git-prompt.sh ]; then
+#  . /usr/local/git/contrib/completion/git-prompt.sh
+#fi
 
-if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
-  . /usr/local/git/contrib/completion/git-completion.bash
-fi
-
-if [ -f /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash ]; then
-  . /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
-fi
-
-if [ -f /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh ]; then
-  . /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
-fi
-
-if [ -f /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-prompt.sh ]; then
-  . /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-prompt.sh
-fi
-
-if [ -f /usr/local/git/contrib/completion/git-prompt.sh ]; then
-  . /usr/local/git/contrib/completion/git-prompt.sh
-fi
-
-if [ -f /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-completion.bash ]; then
-  . /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-completion.bash
-fi
+#if [ -f /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-completion.bash ]; then
+#  . /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-completion.bash
+#fi
