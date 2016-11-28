0cad502e0d1092484b2135a7a98dcd3cd9eb1e75
Patrick Farrell <mr.patfarrell@gmail.com>
2016-11-27 21:44:43 -0800

    bemused

---
+++ bemused/env.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+export BEMUSED_SESSION_SECRET=da81d31727a347c29340b9ac6228e98a
+export BEMUSED_HOST=http://patf.net
+export BEMUSED_UPLOAD_PATH=~/proj/bemused/public/mp3s/var/share2/music/mp3/cd_collection
