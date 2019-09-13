733a16a9fea7acd067fcff2c8e26b6d2d2625e10
Patrick Farrell <patrick.farrell@datarobot.com>
2019-09-12 21:58:11 -0700

    remove broken git prompt, shorten dir, and remove unused stuff

--- git/env.sh
+++ git/env.sh
@@ -1,32 +1,3 @@
 alias gg='grep -R --line-number --exclude-dir .git --color'
 alias gpr='git pull --rebase'
 alias gnb='git checkout -b'
-
-
-#if [ -f /usr/share/git-core/git-completion.bash ]; then
-#  . /usr/share/git-core/git-completion.bash
-#fi
-#
-#if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
-#  . /usr/local/git/contrib/completion/git-completion.bash
-#fi
-#
-#if [ -f /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash ]; then
-#  . /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
-#fi
-#
-#if [ -f /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh ]; then
-#  . /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
-#fi
-#
-#if [ -f /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-prompt.sh ]; then
-#  . /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-prompt.sh
-#fi
-#
-#if [ -f /usr/local/git/contrib/completion/git-prompt.sh ]; then
-#  . /usr/local/git/contrib/completion/git-prompt.sh
-#fi
-
-#if [ -f /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-completion.bash ]; then
-#  . /usr/local/Cellar/git/2.2.1/etc/bash_completion.d/git-completion.bash
-#fi
