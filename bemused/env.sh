bcf480aa043fe1d0d058a0b0d274ee984c19c213
Patrick Farrell <mr.patfarrell@gmail.com>
2025-06-06 22:08:11 -0400

    remove bad plugins and update others

--- bemused/env.sh
+++ bemused/env.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env sh
 
-export BEMUSED_SESSION_SECRET=da81d31727a347c29340b9ac6228e98a
-export BEMUSED_HOST=http://patf.net
-export BEMUSED_UPLOAD_PATH=~/proj/bemused/public/mp3s/var/share2/music/mp3/cd_collection
+export BEMUSED_SESSION_SECRET=5d66850876ff83c3ec82a6b4b572b56fe06c241c691b93833e22af20bf93fccc
+export BEMUSED_UPLOAD_PATH=~/proj/bemused/public/mp3s
+#export BEMUSED_PATH=/
+
